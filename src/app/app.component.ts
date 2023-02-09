a690e760aea48bad3cbb5b49871e881fbee2acfe
Rhama Krisner <rhamakrisner@gmail.com>
2023-02-09 09:03:21 -0300

    ngInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-title></app-title>
+    <router-outlet></router-outlet>`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('1')
+    }, 5000)
+  }
+  
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
-  public title:string = "Bem vindo!"
+export class TitleComponent implements OnInit, OnChanges{
+  public title: string = "Bem vindo!"
+  
+  constructor() { }
+  
+  ngOnInit(): void { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+      
+  }
+
 }
